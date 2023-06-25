60a73cdbfc45937d9c6b451b599045398dbb513f
netnr <netnr@netnr.com>
2023-06-25 14:47:04 +0800

    douyu

---
+++ vercel/api/douyu.js
@@ -0,0 +1,21 @@
+module.exports = async (req, res) => {
+
+    let roomId = parseInt(req.url.split('/')[2]);
+    if (isNaN(roomId)) {
+        res.status(404).send('Invalid room id');
+    } else {
+        let resp = await fetch(`http://open.douyucdn.cn/api/RoomApi/room/${id}`,
+            {
+                headers: {
+                    Referer: `https://www.douyu.com/${id}`,
+                },
+            });
+        let json = await resp.json();
+        if (json.error == 0) {
+            delete json.data.gift;
+            res.json(json.data);
+        } else {
+            res.status(404).json(json);
+        }
+    }
+}
