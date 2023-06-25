57f956d751c664f886f117672f15f07fe2b088d1
netnr <netnr@netnr.com>
2023-06-25 20:32:21 +0800

    fix node-fetch

--- vercel/api/douyu.js
+++ vercel/api/douyu.js
@@ -1,23 +1,21 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-module.exports = async (req, res) => {
-
+module.exports = (req, res) => {
     let roomId = parseInt(req.url.split('/')[2]);
     if (isNaN(roomId)) {
         res.status(404).send('Invalid room id');
     } else {
-        let resp = await fetch(`http://open.douyucdn.cn/api/RoomApi/room/${roomId}`,
-            {
-                headers: {
-                    Referer: `https://www.douyu.com/${roomId}`,
-                },
-            });
-        let json = await resp.json();
-        if (json.error == 0) {
-            delete json.data.gift;
-            res.json(json.data);
-        } else {
-            res.status(404).json(json);
-        }
+        fetch(`http://open.douyucdn.cn/api/RoomApi/room/${roomId}`, {
+            headers: {
+                Referer: `https://www.douyu.com/${roomId}`,
+            },
+        }).then(resp => resp.json()).then(json => {
+            if (json.error == 0) {
+                delete json.data.gift;
+                res.json(json.data);
+            } else {
+                res.status(404).json(json);
+            }
+        });
     }
 }
